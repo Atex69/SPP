70139ffc51d54abadf30cd13a933bc99e38e572e
Atex69 <atex-69@mail.ru>
2020-04-06 04:16:36 +0300

    5 06

--- main.js
+++ main.js
@@ -322,6 +322,81 @@ $( "#electric" ).hover(function(){
 
     });
 
+
+
+    let initCardSwitcher = function () {
+        let contents = [`  <div class="advWorkContentTop1">
+                        <div class="advWorkContentTop1Img"></div>
+                        <div class="advWorkContentTop1Tittle advTittle">Запустим ваш эфир</div>
+                        <div class="advWorkContentTop1Desc advDesc">Предоставим готовое и настроенное к работе
+                            оборудование. Подключите и наслаждайтесь!
+                        </div>
+                    </div>
+                    <div class="advWorkContentTop2">
+                        <div class="advWorkContentTop2Img"></div>
+                        <div class="advWorkContentTop2Tittle advTittle">Музыка без отчислений</div>
+                        <div class="advWorkContentTop2Desc advDesc">Собственная база функциональной музыки без
+                            отчислений в РАО иВОИС.
+                        </div>
+                    </div>
+                    <div class="advWorkContentTop3">
+                        <div class="advWorkContentTop3Img"></div>
+                        <div class="advWorkContentTop3Tittle advTittle">Юридическая поддержка</div>
+                        <div class="advWorkContentTop3Desc advDesc">Наш юридический отдел в течение суток предоставит
+                            письменный ответ на любые обращения РАО или ВОИС.
+                        </div>
+                    </div>
+                    <div class="advWorkContentBottom1">
+                        <div class="advWorkContentBottom1Img"></div>
+                        <div class="advWorkContentBottom1Tittle advTittle">На связи 24/7</div>
+                        <div class="advWorkContentBottom1Desc advDesc">Наша команда на связи 24 часа в сутки 7 дней в
+                            неделю.
+                        </div>
+                    </div>
+                    <div class="advWorkContentBottom2">
+                        <div class="advWorkContentBottom2Img"></div>
+                        <div class="advWorkContentBottom2Tittle advTittle">Создадим яркий ролик</div>
+                        <div class="advWorkContentBottom2Desc advDesc">Собственная студия позволяет в кратчайшие сроки
+                            разработать продающий рекламный контент.
+                        </div>
+                    </div>
+                    <div class="advWorkContentBottom3">
+                        <div class="advWorkContentBottom3Img"></div>
+                        <div class="advWorkContentBottom3Tittle advTittle">Организуем продажу эфира</div>
+                        <div class="advWorkContentBottom3Desc advDesc">Реклама – мощный инструмент в продвижении бренда!
+                            Мы сделаем ваш эфир прибыльным.
+                        </div>`];
+        let containers = $('.advWorkContentMain');
+
+        let renderUp = (index)=> {
+            let contentCopys = contents.slice();//копия исходного массива
+
+            for (let i = 0; i < index; i++) {
+                contentCopys.push(contentCopys.shift());
+            }
+
+            renders(contentCopys);
+        };
+
+        let renders = (contentCopys) => {
+            containers.empty();
+            for (let i = 0; i < contentCopys.length; i++) {
+                containers.append(contentCopys[i]);
+            }
+        };
+
+        let initUP = () => {
+            for (let i = 0; i < 4; i++) {
+                $('#pricerContentCards-'+(i+1)).click(() => {
+                    renderUp(i);
+                })
+            }
+        };
+
+        initUP();
+        renderUp(0);
+        initCardSwitcher();
+    };
 });
 
 
